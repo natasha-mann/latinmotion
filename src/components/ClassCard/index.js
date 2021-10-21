dfc8952e71d9aafc489943c263f9c1118901a94d
Natasha Mann <natasha.s.mann@gmail.com>
2021-10-21 14:48:44 +0100

    added class cards to render

--- src/components/ClassCard/index.js
+++ src/components/ClassCard/index.js
@@ -1,42 +1,19 @@
-import React, { useState } from "react";
-
 import "./ClassCard.css";
 import "../../App.css";
 
 const ClassCard = (props) => {
-  const [display, setDisplay] = useState("hidden");
-  const showDiv = (e) => {
-    e.preventDefault();
-    setDisplay("displayed");
-  };
-
-  const hideDiv = (e) => {
-    e.preventDefault();
-    setDisplay("hidden");
-  };
-
   return (
-    <a
-      href={props.link}
-      className={[`card-container-${props.size}`, `card-container`].join(" ")}
-    >
-      <div
-        className={[`card-image-${props.size}`, `card-image`].join(" ")}
-        style={{
-          backgroundImage: "url(" + props.image + ")",
-        }}
-        onMouseEnter={(e) => showDiv(e)}
-        onMouseLeave={(e) => hideDiv(e)}
-      >
-        <div className={`${display} image-overlay`}>{props.message}</div>
-      </div>
+    <div className="class-card">
+      <img src={props.url} alt={props.alt} className="class-image" />
       <div className="card-body">
-        <div className="card-link" href="/">
-          <h3 className={`card-title-${props.size}`}>{props.name}</h3>
-        </div>
+        <a
+          href="https://latinmotion.as.me/schedule.php"
+          className="class-book-btn"
+        >
+          BOOK NOW
+        </a>
       </div>
-      <div>{props.price && <p className="price-text">£{props.price}</p>}</div>
-    </a>
+    </div>
   );
 };
 
--- src/sections/Classes/index.js
+++ src/sections/Classes/index.js
@@ -1,26 +1,108 @@
+import { useState } from "react";
+
+import ClassCard from "../../components/ClassCard";
 import FlexContainer from "../../components/FlexContainer";
 import Title from "../../components/Title";
 import "./Classes.css";
 
+import jayneSaturday from "../../images/jayne-saturday.jpeg";
+
 const Classes = () => {
+  const [displayVenues, setDisplayVenues] = useState("displayed");
+  const [displayClasses, setDisplayClasses] = useState("hidden");
+  const [classes, setClasses] = useState([]);
+
+  const classData = [
+    {
+      url: jayneSaturday,
+      alt: "Jayne Turpin Ladies Styling Class",
+      location: "bearwood",
+    },
+    {
+      url: jayneSaturday,
+      alt: "Jayne Turpin Ladies Styling Class",
+      location: "bearwood",
+    },
+    {
+      url: jayneSaturday,
+      alt: "Jayne Turpin Ladies Styling Class",
+      location: "bearwood",
+    },
+
+    {
+      url: jayneSaturday,
+      alt: "Jayne Turpin Ladies Styling Class",
+      location: "solihull",
+    },
+  ];
+
+  const renderClasses = (event) => {
+    setDisplayVenues("hidden");
+
+    const classArray = classData.filter(
+      (each) => each.location === event.target.id
+    );
+
+    setClasses(classArray);
+
+    setDisplayClasses("displayed");
+  };
+
   return (
     <FlexContainer padding="true" id="classes">
       <div className="classes-container">
         <Title text="CLASSES" />
-        <div className="class-cards-container">
+        <div className={`${displayVenues} venue-cards-container`}>
           <div className="venue-card">
             <div className="venue-name">BEARWOOD CLASSES</div>
-            <button className="view-btn">VIEW MORE</button>
+            <button className="view-btn" id="bearwood" onClick={renderClasses}>
+              VIEW MORE
+            </button>
           </div>
           <div className="venue-card">
             <div className="venue-name">SOLIHULL WEDNESDAYS</div>
-            <button className="view-btn">VIEW MORE</button>
+            <button className="view-btn" id="solihull" onClick={renderClasses}>
+              VIEW MORE
+            </button>
           </div>
           <div className="venue-card">
             <div className="venue-name">VIRTUAL CLASSES</div>
-            <button className="view-btn">VIEW MORE</button>
+            <button className="view-btn" id="virtual" onClick={renderClasses}>
+              VIEW MORE
+            </button>
           </div>
         </div>
+
+        {displayClasses === "displayed" && (
+          <div className={`${displayClasses} class-cards-container`}>
+            <div className="venue-buttons-container">
+              <button
+                id="bearwood"
+                onClick={renderClasses}
+                className="venue-card-small"
+              >
+                <div className="venue-name-small">BEARWOOD CLASSES</div>
+              </button>
+              <button
+                id="solihull"
+                onClick={renderClasses}
+                className="venue-card-small"
+              >
+                <div className="venue-name-small">SOLIHULL WEDNESDAYS</div>
+              </button>
+              <button
+                id="virtual"
+                onClick={renderClasses}
+                className="venue-card-small"
+              >
+                <div className="venue-name-small">VIRTUAL CLASSES</div>
+              </button>
+            </div>
+            {classes.map((each, index) => {
+              return <ClassCard url={each.url} alt={each.alt} key={index} />;
+            })}
+          </div>
+        )}
       </div>
     </FlexContainer>
   );
